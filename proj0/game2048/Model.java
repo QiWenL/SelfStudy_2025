ca0844d16ec865110de0c5f60cfd4887c96665f5
Q-L <li.qw@berkeley.edu>
2025-02-22 22:40:58 -0500

    finished initial functions before Main Task

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @QL
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,12 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for (int row = 0; row <= b.size()-1; row= row+1 ) {
+            for (int col = 0; col <= b.size()-1; col = col + 1) {
+                if (b.tile(row, col) == null)
+                    return true;
+            }
+        }
         return false;
     }
 
@@ -147,8 +152,18 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
-        return false;
+        for (int row = 0; row <= b.size() - 1; row = row + 1) {
+            for (int col = 0; col <= b.size() - 1; col = col + 1) {
+                Tile current_tile = b.tile(row, col);
+                if (current_tile!=null) {
+                    int current_val = b.tile(row, col).value();
+                    if (current_val == MAX_PIECE) {
+                        return true;
+                    }
+                }
+            }
+        }
+    return false;
     }
 
     /**
@@ -158,10 +173,47 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        // case #1 - if there is at least one empty space on the board
+        int size = b.size();
+        if (emptySpaceExists(b)) {
+            return true;
+        }
+        for (int col = 0; col < size; col += 1) {
+            for (int row = 0; row < size; row += 1) {
+                int current_tile = b.tile(col, row).value();
+                //top
+                if (check_tile(b, current_tile, col, row+1)) {
+                    return true;
+                    //bottom
+                    }else if (check_tile(b, current_tile, col, row-1)) {
+                    return true;
+                    //left
+                        } else if (check_tile(b, current_tile, col-1, row)) {
+                            return true;
+                            //right
+                        } else if (check_tile(b, current_tile, col + 1, row)) {
+                    return true;
+                }
+                }
+            }
         return false;
     }
-
+    public static boolean Valid_index(int size, int col, int row){
+        if (row <0 || col<0){
+            return false;
+        }
+        if (row>=size || col>=size){
+            return false;
+        }
+        return true;
+    }
+    public static boolean check_tile (Board b, int current_tile, int col, int row) {
+        if (Valid_index(b.size(), row, col)) {
+            int neighbor_tile = b.tile(col, row).value();
+            return neighbor_tile == current_tile;
+        }
+        return false;
+    }
 
     @Override
      /** Returns the model as a string, used for debugging. */
